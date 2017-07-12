80fd7428a0ec3f13de04db26d75b4d99b10cd445
jamiecjm <jamiecjm@gmail.com>
2017-07-12 13:16:34 +1000

    Display sudoku board using board method

--- sudoku.rb
+++ sudoku.rb
@@ -12,4 +12,52 @@ class Sudoku
     @sudoku = @sudoku.each_slice(9).to_a
   end
 
+  # Display the sudoku board in a 9x9 grid with nine 3x3 sub-grids
+  # End result should be:
+  	# 0 1 2 | 3 4 5 | 6 7 8  
+	# 0 1 2 | 3 4 5 | 6 7 8 		
+	# 0 1 2 | 3 4 5 | 6 7 8 
+	# ---------------------		
+	# 0 1 2 | 3 4 5 | 6 7 8 		
+	# 0 1 2 | 3 4 5 | 6 7 8 	
+	# 0 1 2 | 3 4 5 | 6 7 8 	
+	# ---------------------
+	# 0 1 2 | 3 4 5 | 6 7 8 		
+	# 0 1 2 | 3 4 5 | 6 7 8 	
+	# 0 1 2 | 3 4 5 | 6 7 8
+  def board
+
+  	# counter for the row and cell numbers
+    cell_counter = 0
+    row_counter = 0
+
+
+    puts "----------------------"
+    @sudoku.each do |row|
+
+      # row_counter indicates the row number/array index of current loop 
+      row_counter += 1  
+      row.each do |cell|
+
+      	# cell_counter indicates the cell number/index in current array
+        cell_counter += 1
+
+        # display underscore to indicate an unsolved grid
+        cell = "_" if cell == 0
+
+        # print the number if the number not 0
+        print "#{cell} "
+
+        # print '|' for every 3 numbers to display to 3x3 sub grid
+        print "|" if cell_counter%3 == 0
+
+        # start a new row for every 9 numbers
+        puts "" if cell_counter%9 == 0
+      end
+
+      # display dotted lines for every 3 rows 
+      puts "----------------------" if row_counter%3 == 0
+    end 
+  end
+
 end
